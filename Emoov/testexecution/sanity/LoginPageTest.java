93d7d0e2166d3bf0730525ab91852e4a16131441
sanket <sanket@SANKET-PC.isynergy.com>
2018-10-05 15:13:30 +0530

    Emoov Assignment -final completion on EOD @05/10/2018

--- Emoov/testexecution/sanity/LoginPageTest.java
+++ Emoov/testexecution/sanity/LoginPageTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -17,14 +18,14 @@ public class LoginPageTest {
 	boolean flag;
 	public static TestData data = new TestData();
 
-	@BeforeClass
+	@BeforeTest
 	public void readTestData() {
 		data.readExcelData();
 		// System.out.println(data.USERNAME);
 		// System.out.println(data.PASSWORD);
 	}
 
-	@BeforeTest
+	@BeforeSuite
 	public void init() {
 		driver = BrowserFactory.createDriver("chrome");
 		if (driver == null) {
